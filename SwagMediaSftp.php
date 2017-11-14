4305d769ff99627cdd30a3ac3f7a539e6c3e1604
Philip Gatzka <p.gatzka@shopware.com>
2017-11-14 12:04:33 +0100

    Remove unecessary methods

--- SwagMediaSftp.php
+++ SwagMediaSftp.php
@@ -9,38 +9,6 @@
 
 namespace SwagMediaSftp;
 
-require __DIR__ . "/vendor/autoload.php";
-
 use Shopware\Components\Plugin;
-use Shopware\Components\Plugin\Context\ActivateContext;
-use Shopware\Components\Plugin\Context\DeactivateContext;
-use Shopware\Components\Plugin\Context\InstallContext;
-use Shopware\Components\Plugin\Context\UninstallContext;
-use Shopware\Components\Plugin\Context\UpdateContext;
 
-class SwagMediaSftp extends Plugin
-{
-    public function update(UpdateContext $context)
-    {
-        $context->scheduleClearCache(InstallContext::CACHE_LIST_DEFAULT);
-        parent::update($context);
-    }
-
-    public function activate(ActivateContext $context)
-    {
-        $context->scheduleClearCache(InstallContext::CACHE_LIST_DEFAULT);
-        parent::activate($context);
-    }
-
-    public function deactivate(DeactivateContext $context)
-    {
-        $context->scheduleClearCache(InstallContext::CACHE_LIST_DEFAULT);
-        parent::deactivate($context);
-    }
-
-    public function uninstall(UninstallContext $context)
-    {
-        $context->scheduleClearCache(InstallContext::CACHE_LIST_DEFAULT);
-        parent::uninstall($context);
-    }
-}
+class SwagMediaSftp extends Plugin {}
